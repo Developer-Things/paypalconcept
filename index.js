d93d82a9769bed7b1b4989af85fd1e3441b558c9
Developer-Things <101121374+Developer-Things@users.noreply.github.com>
2022-05-20 20:58:22 +0530

    update

--- index.js
+++ index.js
@@ -10,84 +10,82 @@ paypal.configure({
     // "EEfDpykhVxisFenPeWZQ0sKIO8QaLnzeDJ6JtZhvuN7eOkBZFbbJYEYLDjcVAHKpwf1ofulN1GCfXIAv"
 });
 
+const app = express();
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
 
 app.post('/pay', (req, res) => {
-     const create_payment_json = {
-      "intent": "sale",
-      "payer": {
-          "payment_method": "paypal"
-      },
-      "redirect_urls": {
-          "return_url": "http://localhost:3000/success",
-          "cancel_url": "http://localhost:3000/cancel"
-      },
-      "transactions": [{
-          "item_list": {
-              "items": [{
-                  "name": "Red Sox Hat",
-                  "sku": "001",
-                  "price": "1.00",
-                  "currency": "USD",
-                  "quantity": 1
-              }]
-          },
-          "amount": {
-              "currency": "USD",
-              "total": "1.00"
-          },
-          "description": "Hat for the best team ever"
-      }]
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/success",
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Red Sox Hat",
+                "sku": "001",
+                "price": "25.00",
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        },
+        "description": "Hat for the best team ever"
+    }]
+};
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+  if (error) {
+      throw error;
+  } else {
+      for(let i = 0;i < payment.links.length;i++){
+        if(payment.links[i].rel === 'approval_url'){
+          res.redirect(payment.links[i].href);
+        }
+      }
+  }
+});
+
+});
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        }
+    }]
   };
-  
-  paypal.payment.create(create_payment_json, function (error, payment) {
+
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
     if (error) {
+        console.log(error.response);
         throw error;
     } else {
-        for(let i = 0;i < payment.links.length;i++){
-          if(payment.links[i].rel === 'approval_url'){
-            res.redirect(payment.links[i].href);
-          }
-        }
+        console.log(JSON.stringify(payment));
+        res.send('Success');
     }
-  });
-  
-  });
-  
-  app.get('/success', (req, res) => {
-    const payerId = req.query.PayerID;
-    const paymentId = req.query.paymentId;
-  
-    const execute_payment_json = {
-      "payer_id": payerId,
-      "transactions": [{
-          "amount": {
-              "currency": "USD",
-              "total": "1.00"
-          }
-      }]
-    };
-  
-    paypal.payment.execute(paymentId, execute_payment_json, 
-        function (error, payment) {
-      if (error) {
-          console.log(error.response);
-          console.log('err')
-          throw error;
-      } else {
-          console.log(JSON.stringify(payment));
-          console.log('success' )
-          res.send('Success');
-      }
-  });
-  });
+});
+});
 
+app.get('/cancel', (req, res) => res.send('Cancelled'));
 
-  app.get('/cancel', (req, res) => res.send('Cancelled'));
-
-
-app.listen(4000, () => console.log('server started'))
+app.listen(3000, () => console.log('Server Started'));
 
 // Email ID:
 // sb-blgl478597960@business.example.com
